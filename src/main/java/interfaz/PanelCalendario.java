bf1439331847b444d6c9a0562303c9ba2f58b7a1
Bindhza <nexxhurclan@gmail.com>
2025-07-12 03:14:30 -0400

    Implementacion del calendario (falta desarrollar)

--- src/main/java/interfaz/PanelCalendario.java
+++ src/main/java/interfaz/PanelCalendario.java
@@ -1,4 +1,30 @@
 package interfaz;
 
-public class PanelCalendario {
+import Logica.Enums.Dia;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelCalendario extends JPanel {
+
+    String[] dias = {"Lunes","Martes","Miercoles","Jueves","Viernes"};
+
+    public PanelCalendario(){
+        setLayout(new FlowLayout());
+        titulo();
+        cuerpo();
+    }
+
+    private void cuerpo() {
+        JTable = new JTable(null, dias);
+    }
+
+    private void titulo(){
+        Font fuente = new Font("Arial", Font.BOLD, 70);
+        JLabel title = new JLabel("Sistema de clases Particulares");
+        title.setForeground(Color.white);
+        title.setFont(fuente);
+        this.add(title);
+        title.setVisible(true);
+    }
 }
